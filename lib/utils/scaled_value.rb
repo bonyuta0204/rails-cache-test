02ed7f81ce00e3c9640bf646cebb9b06de3b1d8d
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-06-22 23:08:20 +0900

    Add scaled value

--- lib/utils/scaled_value.rb
+++ lib/utils/scaled_value.rb
@@ -11,4 +11,19 @@ class ScaledValue
 
     @value * (10**input_scale)
   end
+
+  def formatted_string(display_scale: 0, precision: 0)
+    if display_scale > 12
+      raise "display scale should be smaller than 12"
+    end
+
+    numeric_value = actual_value / (10 ** display_scale)
+    # set adequate precision for numeric_value
+    numeric_value = numeric_value.round(precision)
+
+    # add comma to numeric_value
+    formatted_value = numeric_value.to_s(:delimited)
+
+    "#{formatted_value}#{I18n.t("lib.scaled_value.scale.#{display_scale}")}"
+  end
 end
--- spec/lib/utils/scaled_value_spec.rb
+++ spec/lib/utils/scaled_value_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ScaledValue do
     end
 
     it 'returns the correct scaled value' do
-      expect(scaled_value.actual_value).to eq(100000.to_d)
+      expect(scaled_value.actual_value).to eq(100_000.to_d)
     end
 
     context 'when value is nil' do
@@ -18,6 +18,29 @@ RSpec.describe ScaledValue do
       it 'returns nil' do
         expect(scaled_value.actual_value).to be_nil
       end
+    end
+  end
+
+  describe '#formatted_string' do
+    let(:value) { 1000 }
+    let(:input_scale) { 6 }
+
+    before do
+      I18n.locale = :ja
+    end
+
+    it 'returns the formatted string with default precision' do
+      expect(scaled_value.formatted_string(display_scale: 3)).to eq('1,000,000千')
+    end
+
+    it 'returns the formatted string with specified precision' do
+      expect(scaled_value.formatted_string(display_scale: 3, precision: 1)).to eq('1,000,000.0千')
+    end
+
+    it 'raises an error if display scale is greater than 10' do
+      expect do
+        scaled_value.formatted_string(display_scale: 13)
+      end.to raise_error('display scale should be smaller than 12')
     end
   end
 end
